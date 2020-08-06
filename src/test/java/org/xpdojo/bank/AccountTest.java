bd8c384eee9f2b7c50047d6a1bc29743146cbc47
Christopher Davies <cjhd@Christophers-MacBook-Pro.local>
2020-08-06 11:19:50 +0100

    multiple deposits increase the balance incrementally

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -19,4 +19,12 @@ public class AccountTest {
         account.deposit(10);
         assertThat(account.balance()).isEqualTo(10);
     }
+
+    @Test
+    public void multipleDepositsShouldIncreaseTheBalanceByTotalAmount() {
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.balance()).isEqualTo(30);
+    }
 }
